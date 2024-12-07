c24060dbe659db968cae74b5515dde3757f7e227
Juneon Kim <ldskimje@gamil.com>
2024-12-06 18:24:46 -0700

    Creating Dataset

--- readability.py
+++ readability.py
@@ -11,6 +11,7 @@ import name_quality
 import comment_ratio
 import complexity
 import maxlinesfeature
+import readability_score
 
 def check_py2(file):
     try:
@@ -43,7 +44,7 @@ if __name__ == '__main__':
             with zipfile.ZipFile(zfile, 'r') as zip_ref:
                 zip_ref.extractall("temp_dest")
 
-            data = [["#", "Style Guide Adherence", "Variable Name Quality", "Complexity Score", "Max Line", "Comment Ratio"]]
+            data = [["#", "Style Guide Adherence", "Variable Name Quality", "Complexity Score", "Max Line", "Comment Ratio", "Readability"]]
             for root, dirs, files in os.walk("temp_dest"):
                 py_files = [x for x in files if x.endswith('py')]
                 for file in py_files:
@@ -56,18 +57,22 @@ if __name__ == '__main__':
                         match = re.search(r'\d+', file_path)
                         id = int(match.group())
 
-                        adherence_score = '?' if style_guide_adherence.style_adherence(file_path) < 0 else style_guide_adherence.style_adherence(file_path)
-                        complexity_score = complexity.get_complexity(file_path) or '?'
-                        max_line = maxlinesfeature.max_lines(file_path)
-                        with open(file_path, 'r') as file:
-                            content = file.read()
-                            var_name_score = name_quality.calculate_quality(content)
-                            #commnet_ratio = comment_ratio.get_ratio(content)
+                        try:
+                            adherence_score = '?' if style_guide_adherence.style_adherence(file_path) < 0 else style_guide_adherence.style_adherence(file_path)
+                            complexity_score = complexity.get_complexity(file_path) or '?'
+                            max_line = maxlinesfeature.max_lines(file_path)
+                            readability = readability_score.get_readability(file_path)
+                            with open(file_path, 'r') as file:
+                                content = file.read()
+                                var_name_score = name_quality.calculate_quality(content)
+                                commnet_ratio = comment_ratio.get_ratio(content)
 
-                        result = [id, adherence_score, var_name_score, complexity_score, max_line]
-                        data.append(result)
-
-            output_file = "code_readability.csv"
+                            result = [id, adherence_score, var_name_score, complexity_score, max_line, commnet_ratio, readability]
+                            data.append(result)
+                        except Exception as e:
+                            print(f"Error processing file {file_path}: {e}")
+                            continue
+            output_file = "code_readability2.csv"
             with open(output_file, mode="w", newline="", encoding="utf-8") as file:
                 writer = csv.writer(file)
                 writer.writerows(data)
@@ -79,5 +84,7 @@ if __name__ == '__main__':
             print(f"Error: The file '{sys.argv[1]}' was not found.")
         except OSError as e:
             print(f"Error: {e}")
+        except SyntaxError:
+            print(f"Error: The file '{sys.argv[1]}' has syntax error.")
     else:
         print("Usage: python readability.py <filename.zip>")
